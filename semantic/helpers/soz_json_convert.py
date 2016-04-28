21dfc2e1d29f9ca61e020c4208f0a25edaae19e0
Aleksandr Korovin <aw3s0me@rocketmail.com>
2016-04-28 19:37:53 +0200

    Fixed code to download and unzip thesoz #14

--- semantic/helpers/soz_json_convert.py
+++ semantic/helpers/soz_json_convert.py
@@ -3,18 +3,28 @@ from rdflib.plugins.parsers.notation3 import TurtleParser
 from rdflib.namespace import RDF, FOAF, DC, DCTERMS, NamespaceManager, Namespace
 import re
 import json
+import urllib
+import zipfile
 
 thesoz_ns = Namespace("http://lod.gesis.org/thesoz/ext/")
 
+def download_thesoz():
+    thesoz_addr = 'http://www.etracker.de/lnkcnt.php?et=qPKGYV&url=http://www.gesis.org/fileadmin/upload/dienstleistung/tools_standards/thesoz_skos_turtle.zip&lnkname=fileadmin/upload/dienstleistung/tools_standards/thesoz_skos_turtle.zip'
+    zip_file_name = 'thesoz_0_93.zip'
+
+    urllib.urlretrieve(thesoz_addr, zip_file_name)
+
+    fh = open(zip_file_name, 'rb')
+    z = zipfile.ZipFile(fh)
+    z.extractall()
+    fh.close()
+
+download_thesoz()
+
 g = Graph()
-# ns_manager = NamespaceManager(g)
-# ns_manager.bind('thesoz', thesoz_ns)
-# ns_manager.bind('rdf', RDF)
 
 skos_ttl = open('thesoz_0_93.ttl', 'r')
 result = g.parse(skos_ttl, format='turtle')
-
-#classes = g.triples([None, None, thesoz_ns.Classification])
 
 query = """
         PREFIX thesoz: <http://lod.gesis.org/thesoz/ext/>
